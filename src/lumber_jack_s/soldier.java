668493869e34b156e905ab770dd80c17f68da2d8
azane <azane@nnu.edu>
2017-01-18 10:59:01 -0600

    adds code to move along gradient, includes enemy archon in gradient.

--- src/lumber_jack_s/soldier.java
+++ src/lumber_jack_s/soldier.java
@@ -29,9 +29,6 @@ public strictfp class soldier extends RobotPlayer{
 
                 MapLocation myLocation = rc.getLocation();
 
-                // See if there are any nearby enemy robots
-//                RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
-//
 //                // If there are some...
 //                if (robots.length > 0) {
 //                    // And we have enough bullets, and haven't attacked yet this turn...
@@ -40,25 +37,18 @@ public strictfp class soldier extends RobotPlayer{
 //                        rc.fireSingleShot(rc.getLocation().directionTo(robots[0].location));
 //                    }
 //                }
-//
-//                // If enemy archon is being broadcasted, go to that location -- 10 == x_value, 11 == y_value
-//            	Direction dirToMove = randomDirection();
-//                if (!RobotPlayer.foundEnemyArchon()) {
-//                	dirToMove = RobotPlayer.huntEnemyArchon();
-//                }
-//                tryMove(dirToMove);
-//
-//                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
-//                Clock.yield();
 
                 // TODO figure out if this takes up too much bytecode, if it does, take a random sample of nearby bots.
                 //          over a few turns, the collective result will effectively be an average over all bots.
+                // TODO softcode all the curve parameters.
 
                 // Sense all nearby robots.
                 RobotInfo[] robots = rc.senseNearbyRobots();
 
-                // Init the gradient vector over which we'll sum.
-                double[] gradient = new double[] {0., 0.};
+                // Init the gradient vector over which we'll sum. Keep track of enemy/friendly so we know
+                //  if it's safe to shoot, and in which direction.
+                double[] friendlyGradient = new double[] {0., 0.};
+                double[] enemyGradient = new double[] {0., 0.};
 
                 // Iterate nearby bots, calculating the gradient by which this bot should move.
                 for (int i = 0; i < robots.length; ++i) {
@@ -66,10 +56,10 @@ public strictfp class soldier extends RobotPlayer{
                         // Use the same doughnut function for all enemies, for now.
                         // Add the gradient from this bot.
                         SjxMath.elementwiseSum(
-                                gradient,
+                                enemyGradient,
                                 SjxMath.doughnutDerivative(myLocation, robots[i].location,
-                                        4, -10,
-                                        10, 1, true),
+                                        2, -10,
+                                        7, 2, true),
                                 false
                         );
                     }
@@ -80,9 +70,9 @@ public strictfp class soldier extends RobotPlayer{
                             case TANK:
                                 // Use a standard gaussian curve for friendlies.
                                 SjxMath.elementwiseSum(
-                                        gradient,
+                                        friendlyGradient,
                                         SjxMath.gaussianDerivative(myLocation, robots[i].location,
-                                                5, 1),
+                                                7, 1.5),
                                         false
                                 );
                                 break;
@@ -90,10 +80,35 @@ public strictfp class soldier extends RobotPlayer{
                     }
                 }
 
-                // TODO Add the enemy archon location to the gradient.
+                // Add the enemy archon location to the gradient. Use a wide gaussian.
+                MapLocation enemyArchonLocation = getEnemyArchonLocation();
+                if (enemyArchonLocation != null) {
+                    SjxMath.elementwiseSum(
+                            enemyGradient,
+                            SjxMath.gaussianDerivative(myLocation, getEnemyArchonLocation(),
+                                    50, 6),
+                            false
+                    );
+                }
 
-                // TODO Move in the direction of the gradient.
 
+                // TODO Verify that the enemy gradient opposes the friendly gradient by at least 45 degrees.
+                // TODO  Fire toward the gradient if so.
+
+
+                // Move in the direction of the gradient.
+
+                // Convert and scale.
+                double[] gradient = SjxMath.elementwiseSum(enemyGradient, friendlyGradient, false);
+                double s = .3;
+                float x = (float) (gradient[0] * s);
+                float y = (float) (gradient[1] * s);
+
+                // Add scaled gradient to myLocation coordinates.
+                MapLocation gradientDestination = new MapLocation(myLocation.x + x, myLocation.y + y);
+
+                // Move toward the new vector.
+                tryMove(myLocation.directionTo(gradientDestination));
 
                 Clock.yield();
 

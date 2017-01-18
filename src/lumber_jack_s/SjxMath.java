7418ff1cc401b9d1bf96109099e75370f2cdbc45
azane <azane@nnu.edu>
2017-01-17 22:13:20 -0600

    adds some integration of the microgradient calculations into the soldier.

---
+++ src/lumber_jack_s/SjxMath.java
@@ -0,0 +1,85 @@
+package lumber_jack_s;
+import battlecode.common.*;
+
+/**
+ * A set of mathematical functions for use in the SgtAjax battlecode bot.
+ */
+public strictfp class SjxMath {
+
+    public static double[] elementwiseSum(double[] x1, double[] x2, boolean negate) {
+
+        double[] sumVector = new double[x1.length];
+
+        double sign = 1.;
+        if (negate) {
+            sign *= -1.;
+        }
+
+        for (int i = 0; i < x1.length; ++i) {
+            sumVector[i] = x1[i] + x2[i]*sign;
+        }
+
+        return sumVector;
+    }
+
+    public static double euclideanSquaredDistance(double[] x1, double[] x2) {
+
+        double totalDif = 0;
+
+        for (int i = 0; i < x1.length; ++i) {
+            totalDif += Math.pow(x1[i] - x2[i], 2.);
+        }
+
+        return totalDif;
+    }
+
+    /**
+     * Returns the value at x for a given gaussian. The variance is defined isotropically, i.e. the same for all axes.
+     */
+    public static double gaussian(double[] x, double[] mean, double standardDeviation, double scale) {
+
+        double esd = euclideanSquaredDistance(x, mean);
+
+        return scale * Math.exp(-esd/2*Math.pow(standardDeviation, 2.));
+    }
+
+    public static double[] gaussianDerivative(double[] x, double[] mean, double standardDeviation, double scale) {
+
+        double gauss = gaussian(x, mean, standardDeviation, scale);
+        double variance = Math.pow(standardDeviation, 2.);
+
+        double[] partials = new double[x.length];
+
+        for (int i = 0; i < x.length; ++i) {
+            partials[i] = (-(x[i] - mean[i])/variance)*gauss;
+        }
+
+        return partials;
+    }
+
+    public static double[] gaussianDerivative(MapLocation me, MapLocation other,
+                                              double standardDeviation, double scale) {
+        double[] xarray = new double[] {me.x, me.y};
+        double[] meanarray = new double[] {other.x, other.y};
+
+        return gaussianDerivative(xarray, meanarray, standardDeviation, scale);
+    }
+
+    // See https://www.desmos.com/calculator/crogrhzvyq
+    public static double[] doughnutDerivative(MapLocation me, MapLocation other,
+                                              double innerStandardDeviation, double innerScale,
+                                              double outerStandardDeviation, double outerScale,
+                                              boolean outerMinusInner) {
+
+        double[] innerDerivative = gaussianDerivative(me, other, innerStandardDeviation, innerScale);
+        double[] outerDerivative = gaussianDerivative(me, other, outerStandardDeviation, outerScale);
+
+        // The collective derivative is just a difference, as the original function is simply a difference.
+        if (outerMinusInner) {
+            return elementwiseSum(outerDerivative, innerDerivative, true);
+        }
+        else {
+            return elementwiseSum(innerDerivative, outerDerivative, true);
+        }
+    }
+}
--- src/lumber_jack_s/soldier.java
+++ src/lumber_jack_s/soldier.java
@@ -30,25 +30,71 @@ public strictfp class soldier extends RobotPlayer{
                 MapLocation myLocation = rc.getLocation();
 
                 // See if there are any nearby enemy robots
-                RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
+//                RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
+//
+//                // If there are some...
+//                if (robots.length > 0) {
+//                    // And we have enough bullets, and haven't attacked yet this turn...
+//                    if (rc.canFireSingleShot()) {
+//                        // ...Then fire a bullet in the direction of the enemy.
+//                        rc.fireSingleShot(rc.getLocation().directionTo(robots[0].location));
+//                    }
+//                }
+//
+//                // If enemy archon is being broadcasted, go to that location -- 10 == x_value, 11 == y_value
+//            	Direction dirToMove = randomDirection();
+//                if (!RobotPlayer.foundEnemyArchon()) {
+//                	dirToMove = RobotPlayer.huntEnemyArchon();
+//                }
+//                tryMove(dirToMove);
+//
+//                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+//                Clock.yield();
 
-                // If there are some...
-                if (robots.length > 0) {
-                    // And we have enough bullets, and haven't attacked yet this turn...
-                    if (rc.canFireSingleShot()) {
-                        // ...Then fire a bullet in the direction of the enemy.
-                        rc.fireSingleShot(rc.getLocation().directionTo(robots[0].location));
+                // TODO figure out if this takes up too much bytecode, if it does, take a random sample of nearby bots.
+                //          over a few turns, the collective result will effectively be an average over all bots.
+
+                // Sense all nearby robots.
+                RobotInfo[] robots = rc.senseNearbyRobots();
+
+                // Init the gradient vector over which we'll sum.
+                double[] gradient = new double[] {0., 0.};
+
+                // Iterate nearby bots, calculating the gradient by which this bot should move.
+                for (int i = 0; i < robots.length; ++i) {
+                    if (robots[i].getTeam() == enemy) {
+                        // Use the same doughnut function for all enemies, for now.
+                        // Add the gradient from this bot.
+                        SjxMath.elementwiseSum(
+                                gradient,
+                                SjxMath.doughnutDerivative(myLocation, robots[i].location,
+                                        4, -10,
+                                        10, 1, true),
+                                false
+                        );
+                    }
+                    else {
+                        // Only school with other military units.
+                        switch (rc.getType()) {
+                            case SOLDIER:
+                            case TANK:
+                                // Use a standard gaussian curve for friendlies.
+                                SjxMath.elementwiseSum(
+                                        gradient,
+                                        SjxMath.gaussianDerivative(myLocation, robots[i].location,
+                                                5, 1),
+                                        false
+                                );
+                                break;
+                        }
                     }
                 }
 
-                // If enemy archon is being broadcasted, go to that location -- 10 == x_value, 11 == y_value
-            	Direction dirToMove = randomDirection();
-                if (!RobotPlayer.foundEnemyArchon()) {
-                	dirToMove = RobotPlayer.huntEnemyArchon();
-                }
-                tryMove(dirToMove);
+                // TODO Add the enemy archon location to the gradient.
 
-                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+                // TODO Move in the direction of the gradient.
+
+
                 Clock.yield();
 
             } catch (Exception e) {
